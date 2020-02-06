bc98b111879c2b2367a191ae248f34f1fbadb23a
Erick Nunes <51167402+4ntr0m0rph0s@users.noreply.github.com>
2020-02-06 15:08:08 -0300

    Add files via upload

---
+++ loja/Entities/ImportedProduct.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace loja.Entities
+{
+    class ImportedProduct : Product
+    {
+        public double CustomFee { get; set; }
+
+        public ImportedProduct()
+        {
+        }
+
+        public ImportedProduct(string name, double price, double customFee)
+            :base(name,price)
+        {
+            CustomFee = customFee;
+        }
+
+        public new string PriceTag()
+        {
+            return Name
+                + " $ "
+                + TotalPrice().ToString("F2", CultureInfo.InvariantCulture)
+                + " (Customs fee: $ "
+                + CustomFee.ToString("F2", CultureInfo.InvariantCulture)
+                + ")";
+        }
+
+        public double TotalPrice()
+        {
+            return Price + CustomFee;
+        }
+
+    }
+}
---
+++ loja/Entities/UsedProduct.cs
@@ -0,0 +1,32 @@
+﻿using loja.Entities;
+using System;
+using System.Globalization;
+
+namespace loja.Entities
+{
+    class UsedProduct : Product
+    {
+        public DateTime ManufactureDate { get; set; }
+
+        public UsedProduct()
+        {
+        }
+
+        public UsedProduct(string name, double price, DateTime manufactureDate)
+            : base(name, price)
+        {
+            ManufactureDate = manufactureDate;
+        }
+
+        public new string PriceTag()
+        {
+            return Name
+            + " (used) $ "
+            + Price.ToString("F2", CultureInfo.InvariantCulture)
+            + " (Manufacture date: "
+            + ManufactureDate.ToString("dd/MM/yyyy")
+            + ")";
+
+        }
+    }
+}
