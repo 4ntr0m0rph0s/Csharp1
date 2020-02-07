3d2383e94ef515f06ad6619d432b937751ee0aed
4ntr0m0rph0s <erickjaden@gmail.com>
2020-02-07 11:43:49 -0300

    teste

--- ConsoleApplication5/Program.cs
+++ ConsoleApplication5/Program.cs
@@ -10,7 +10,7 @@ namespace ConsoleApplication5
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello darkness my old friend...");
+            Console.WriteLine("Hello erick...");
         }
     }
 }
